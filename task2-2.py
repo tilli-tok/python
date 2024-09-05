810be0d925cd613e82b1d02901d92a6e04d3bada
tilli <Zvada_A_S@itgri.ru>
2024-09-05 21:46:11 +0300

    Первый коммит

---
+++ task2-2.py
@@ -0,0 +1,21 @@
+import turtle as t
+
+t.ht()
+t.speed(10)
+t.pensize(5)
+
+ugol = 4
+
+for r in range(40, 0, -10):
+    for i in range(6):
+        color_value = min(r * 6, 255)
+        t.color(f'#{255:02x}{165:02x}{color_value:02x}')
+        t.fillcolor(f'#{162:02x}{color_value:02x}{255:02x}')
+        t.begin_fill()
+        for i in range(ugol):
+            t.forward(r*3)
+            t.left(360/ugol)
+        t.end_fill()
+        t.rt(60)
+
+t.done()
