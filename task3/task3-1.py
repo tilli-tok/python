314583a8ca81cfdaf1c032e5afd2950079f10243
tilli <Zvada_A_S@itgri.ru>
2024-09-13 12:58:09 +0300

    Задание 2.3

--- task3/task3-1.py
+++ task3/task3-1.py
@@ -1,23 +1,25 @@
 T = input("Введите значение температуры воды: ")
-
-try:
-    T = float(T)
-    T = int(T)
-    if T < 0:
-        print("Озеро замерзло")
-    elif 0 <= T < 10:
-        print("Ледяная вода")
-    elif 10 <= T < 15:
-        print("Жуть как холодно")
-    elif 15 <= T < 18:
-        print("Прохладно, но можно искупаться")
-    elif 18 <= T < 24:
-        print("Кайф")
-    elif 24 <= T < 30:
-        print("Полный кайф")
-    elif 30 <= T < 36:
-        print("Горячая")
-    else:
-        print("Кипяток")
-except:
-    print("Вы ввели не число ")
+while True:
+  try:
+      T = float(T)
+      T = int(T)
+      if T < 0:
+          print("Озеро замерзло")
+      elif 0 <= T < 10:
+          print("Ледяная вода")
+      elif 10 <= T < 15:
+          print("Жуть как холодно")
+      elif 15 <= T < 18:
+          print("Прохладно, но можно искупаться")
+      elif 18 <= T < 24:
+          print("Кайф")
+      elif 24 <= T < 30:
+          print("Полный кайф")
+      elif 30 <= T < 36:
+          print("Горячая")
+      else:
+          print("Кипяток")
+      break
+  except:
+      print("Вы ввели не число ")
+      T = input("Введите значение температуры воды: ")
